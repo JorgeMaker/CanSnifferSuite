c17e0552611142fda1a45f332a049532c814b64d
Jorge Sans Llopis <44008430+JorgeMaker@users.noreply.github.com>
2023-10-15 11:28:06 +0200

    Update LibraryUsageDummyExample.py
    Added detais to conect in normal mode at 125 kbps

--- guiApp/CanBusSnnifer/LibraryUsageDummyExample.py
+++ guiApp/CanBusSnnifer/LibraryUsageDummyExample.py
@@ -11,10 +11,14 @@ if __name__ == '__main__':
     datagram1.extended = False
     datagram1.data = b'\01\02'
 
+    # Connecting to Snnifer at 125 kbps
     snniffer =CanSnifferDevice(portName)
     snniffer.connectSerial()
     snniffer.rebootDevice()
+    snniffer.sendModeCommand('NORMAL')
+    snniffer.sendBitrateCommand('125.000')
 
     while True:
         snniffer.sendDatagram(datagram1)
         time.sleep(1/1000)
+
